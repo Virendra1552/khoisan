f68dbfd48ef51072ba509f8ee0e816f383184a41
sourabh patil <ankitac@noitavonne.com>
2023-08-16 18:03:19 +0530

    changed dummy content

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="khoisan.com">
+    <meta name="description" content="The origins of humanity and its evolution are hotly debated among scientists. This is a version that traces the path from the first appearance of modern man to the present day">
+    <meta name="keyword" content="khoisan, Khoi-San, origins of humanity, history in Southern Africa, click, click language, indigenous groups, southern region of Africa, Bushmen, historical communities, pre-Bantu communities">
     <meta name="viewport" content="The origins of humanity and its evolution are hotly debated among scientists. This is a version that traces the path from the first appearance of modern man to the present day">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $base_url."images/Khoisan-fav.png"; ?>">
     <meta property="og:title" content="Khoisan">
@@ -21,6 +23,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="<?php echo $base_url."css/jquery.fancybox.css"; ?>">
     <style>
       .section-home{
         background-image:linear-gradient(hsla(0, 0%, 0%, 0.2),hsla(0, 0%, 0%, 0.2)), url("images/khoisan-bg.jpg");
@@ -30,6 +33,9 @@
         background-position: center;
         background-repeat: no-repeat;
       }
+      /*.sp{
+        background-color: red;
+      }*/
     </style>
   </head>
   <body>
@@ -44,7 +50,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class="nav-link" href="#home">Home</a>
+                  <a class="nav-link active sp" href="#home">Home</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#about">About</a>
@@ -53,7 +59,7 @@
                   <a class="nav-link" href="#history">History</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#gallery">Gallery</a>
+                  <a class="nav-link" href="#gallery1">Gallery</a>
                 </li>
                 <li class="nav-item d-none">
                   <a class="nav-link" href="#testomonials">Testomonials</a>
@@ -63,7 +69,7 @@
                 </li>
               </ul>
               <form class="d-flex" role="search">                
-                <button class="btn btn-theme" type="submit">Sign In</button>
+                <button class="btn btn-theme" type="button">Sign In</button>
               </form>
             </div>
           </div>
@@ -71,16 +77,16 @@
       </div>
     </section>
 
-    <section class="section-home" id="home">
+    <section class="section-home section" id="home">
       <div class="container">
         <h1 class="text-center">The origins of humanity and its evolution are hotly debated among scientists. This is a version that traces the path from the first appearance of modern man to the present day</h1>
       </div>
     </section>
 
-    <section class="section-about" id="about">
+    <section class="section-about section" id="about">
       <div class="container">
         <div class="head text-center mb-3">
-          <h2>About <span class="text-theme">khoisan</span></h2>
+          <h2>About <span class="text-theme">Khoisan</span></h2>
         </div>
         <div class="row">
           <div class="col-md-6 col-lg-7">
@@ -88,7 +94,7 @@
             <h5>Ancient Roots:</h5>
             <ul>
               <li>The Khoisan people are among the earliest inhabitants of Southern Africa, with a presence dating back tens of thousands of years.</li>
-              <li>The Khoisan people are among the earliest inhabitants of Southern Africa, with a presence dating back tens of thousands of years.</li>
+              <li>One of the most distinctive features of the Khoisan people is their use of click languages, which are characterized by the presence of various click sounds produced with different parts of the mouth.</li>
             </ul>
              <h5>Distinctive Click Languages:</h5>
             <ul>
@@ -110,10 +116,10 @@
       </div>
     </section>
 
-    <section class="section-history" id="history">
+    <section class="section-history section" id="history">
       <div class="container">
         <div class="head text-center mb-3">
-          <h2><span class="text-theme">khoisan</span> History</h2>
+          <h2><span class="text-theme">Khoisan</span> History</h2>
         </div>
         <div class="row">          
           <div class="col-md-6 col-lg-5">
@@ -138,41 +144,57 @@
       </div>
     </section>
 
-    <section class="section-gallery" id="gallery">
+    <section class="section-gallery section" id="gallery1">
       <div class="container">
         <div class="head text-center mb-3">
-          <h2><span class="text-theme">khoisan</span> Gallery</h2>
+          <h2><span class="text-theme">Khoisan</span> Gallery</h2>
         </div>
         <div class="gallery row p-3">
           <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-            <img src="<?php echo $base_url."images/image-1.jpg"; ?>" alt="gallery image">
+            <a href="<?php echo $base_url."images/image-1.jpg"; ?>" class="show_image" data-fancybox="gallery" data-caption="Caption Images 1">
+              <img src="<?php echo $base_url."images/image-1.jpg"; ?>" alt="Image Gallery">
+            </a>
           </div>
           <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-            <img src="<?php echo $base_url."images/image-2.jpg"; ?>" alt="gallery image">
+            <a href="<?php echo $base_url."images/image-2.jpg"; ?>" class="show_image" data-fancybox="gallery" data-caption="Caption Images 1">
+              <img src="<?php echo $base_url."images/image-2.jpg"; ?>" alt="Image Gallery">
+            </a>
           </div>
           <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-            <img src="<?php echo $base_url."images/image-3.jpg"; ?>" alt="gallery image">
+            <a href="<?php echo $base_url."images/image-3.jpg"; ?>" class="show_image" data-fancybox="gallery" data-caption="Caption Images 1">
+              <img src="<?php echo $base_url."images/image-3.jpg"; ?>" alt="Image Gallery">
+            </a>
           </div>
           <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-            <img src="<?php echo $base_url."images/image-4.jpg"; ?>" alt="gallery image">
+            <a href="<?php echo $base_url."images/image-4.jpg"; ?>" class="show_image" data-fancybox="gallery" data-caption="Caption Images 1">
+              <img src="<?php echo $base_url."images/image-4.jpg"; ?>" alt="Image Gallery">
+            </a>
           </div>
           <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-            <img src="<?php echo $base_url."images/image-5.jpg"; ?>" alt="gallery image">
+            <a href="<?php echo $base_url."images/image-5.jpg"; ?>" class="show_image" data-fancybox="gallery" data-caption="Caption Images 1">
+              <img src="<?php echo $base_url."images/image-5.jpg"; ?>" alt="Image Gallery">
+            </a>
           </div>
           <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-            <img src="<?php echo $base_url."images/image-6.jpg"; ?>" alt="gallery image">
+            <a href="<?php echo $base_url."images/image-6.jpg"; ?>" class="show_image" data-fancybox="gallery" data-caption="Caption Images 1">
+              <img src="<?php echo $base_url."images/image-6.jpg"; ?>" alt="Image Gallery">
+            </a>
           </div>
           <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-            <img src="<?php echo $base_url."images/image-7.jpg"; ?>" alt="gallery image">
+            <a href="<?php echo $base_url."images/image-7.jpg"; ?>" class="show_image" data-fancybox="gallery" data-caption="Caption Images 1">
+              <img src="<?php echo $base_url."images/image-7.jpg"; ?>" alt="Image Gallery">
+            </a>            
           </div>
           <div class="col-sm-6 col-md-4 col-lg-3 p-2">
-            <img src="<?php echo $base_url."images/kohisan.jpeg"; ?>" alt="gallery image">
+            <a href="<?php echo $base_url."images/kohisan.jpeg"; ?>" class="show_image" data-fancybox="gallery" data-caption="Caption Images 1">
+              <img src="<?php echo $base_url."images/kohisan.jpeg"; ?>" alt="Image Gallery">
+            </a>           
           </div>
         </div>
-      </div>
-    </section>
+    </div>
+  </section>
 
-    <section class="section-contact" id="contact">
+    <section class="section-contact section" id="contact">
       <div class="container">
         <div class="head text-center mb-3">
           <h2>Contact Us</h2>
@@ -203,7 +225,7 @@
               <textarea name="message" rows="5" id="message"></textarea>
             </div>
             <div class="full my-2 ">
-              <button type="button" id="btncontactsubmit" class="btn-theme d-inline-block">Submit</button>
+               <button class="btn btn-theme" type="submit">Submit</button>
             </div>
           </form>
         </div>
@@ -214,7 +236,7 @@
       </div>
     </section>
 
-    <section class="section-footer">
+    <section class="section-footer section">
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-10">
@@ -232,13 +254,13 @@
                 <h5 class="fw-bold">Connect With Us</h5>
                 <h6 class="d-none"> Reach us at <a href="#" class="text-decoration-underline">support@gist.com</a> or through our <a href="#" class="text-decoration-underline">online contact</a> form.</h6>
                 <div class="">
-                  <a href="https://www.facebook.com/people/Silocloud/100081311599960/" class="footer-icon"><i class="bx bxl-facebook-circle"></i></a>
-                  <a href="https://twitter.com/cloud_silo" class="footer-icon"><i class="bx bxl-twitter"></i></a>
-                  <a href="https://www.linkedin.com/company/silocloud" class="footer-icon"><i class="bx bxl-linkedin"></i></a>
-                  <a href="https://www.youtube.com/channel/UCkzrRrUo6NkzkFUvMpfU0Iw" class="footer-icon"><i class="bx bxl-youtube"></i></a>
+                  <a href="#" class="footer-icon"><i class="bx bxl-facebook-circle"></i></a>
+                  <a href="#" class="footer-icon"><i class="bx bxl-twitter"></i></a>
+                  <a href="#" class="footer-icon"><i class="bx bxl-linkedin"></i></a>
+                  <a href="#" class="footer-icon"><i class="bx bxl-youtube"></i></a>
                 </div>
               </div>
-              <div class="col-md-6">
+              <div class="col-md-6 d-none">
                 <h5 class="fw-bold">Address</h5>
                 <h5 class="d-none">
                   Noitavonne <br>
@@ -249,7 +271,7 @@
                   <div class="input-group d-none">
                     <input type="text" class="form-control" name="email_address" id="email_address" placeholder="Enter your email" style="background-color:#fff;" autocomplete="off" fdprocessedid="xvbkxs">
                     <span class="input-group-btn">
-                      <button class="btn-default" type="button" id="SubscribeMail" fdprocessedid="5uy1s">Subscribe</button>
+                      <button class="btn-theme" type="button" id="SubscribeMail" fdprocessedid="5uy1s">Subscribe</button>
                     </span>
                   </div>
                 </form>
@@ -260,9 +282,17 @@
       </div>
     </section>
 
+    <div class="bottom-arrow">
+      <a href="#" class="bottom-arrow-div d-flex align-items-center justify-content-center">
+        <i class="bx bx-up-arrow-alt"></i>
+      </a>
+    </div>
+
    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="script.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+     <script src="<?php echo $base_url."css/jquery.fancybox.js"; ?>"></script> 
 
     <script>
 
@@ -276,6 +306,40 @@
         $("#section-header").addClass("section-header");
       }
     });
+    </script>
+
+    <script>
+      $(".show_image").fancybox({
+        });
+
+      const sections = document.querySelectorAll('.section');
+      const navLinks = document.querySelectorAll('.nav-link');
+// Function to add or remove active class based on scroll position
+      // navLinks.forEach(function(e){
+      //   e.addEventListener("click", function(){
+      //     console.log(navLinks);
+      //     $(".nav-link").removeClass("sp");
+      //     $(this).addClass("sp");
+      //   });
+      // });
+      function setActiveNav() {
+        sections.forEach((section, index) => {
+          const rect = section.getBoundingClientRect();
+          if (rect.top <= window.innerHeight / 1 && rect.bottom >= window.innerHeight / 1) {
+            navLinks.forEach((navLink) => {
+              navLink.classList.remove('active');
+            });
+            navLinks[index].classList.add('active');
+          }
+        });
+      }
+
+// Event listener for window scroll
+      window.addEventListener('scroll', setActiveNav);
+
+// Initial call to set active navigation based on initial scroll position
+      setActiveNav();
+
     </script>
   </body>
 </html>
