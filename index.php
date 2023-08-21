b66a346d76740edaf736f391435a52d90489baf9
sourabh patil <ankitac@noitavonne.com>
2023-08-21 11:58:39 +0530

    added PWA files

--- index.php
+++ index.php
@@ -27,6 +27,8 @@
     <link rel="stylesheet" href="<?php echo $base_url."css/jquery.fancybox.css"; ?>">
     <link rel="stylesheet" href="<?php echo $base_url."css/toastr.min.css"; ?>">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="manifest" href="manifest.webmanifest">
+
     <style>
       .section-home{
         background-image:linear-gradient(hsla(0, 0%, 0%, 0.2),hsla(0, 0%, 0%, 0.2)), url("images/khoisan-bg.jpg");
@@ -292,7 +294,7 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="script.js"></script>
+    <script src="index.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="<?php echo $base_url."js/jquery.fancybox.js"; ?>"></script> 
